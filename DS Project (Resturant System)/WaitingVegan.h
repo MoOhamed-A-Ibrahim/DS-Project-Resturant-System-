e6e581a66bb07074ae799fdb5bea677d8b29b8c6
Mohamed A. Ibrahim <s-mohamed.elrehim@zewailcity.edu.eg>
2025-04-11 18:11:59 +0200

    WaitingVeganQueue

---
+++ DS Project (Resturant System)/WaitingVegan.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Order.h"
+#include "Node.h"
+
+
+class WaitingVegan : Order
+{
+	int type = 1;
+	bool available;
+	int Count = 0;
+	Node* front = NULL;
+	Node* rear = NULL;
+
+public:
+	WaitingVegan(int t = 1, double p = 0);
+	virtual int getType() const override;
+	virtual void setType(int t) override;
+	virtual bool isAvailable() override;
+	int getCount() const;
+	void setCount(int c);
+
+	// Queue Operations
+
+	void Assign(double P, string S); // Enquqe Operation
+
+	Node* Serve();            //Dequeue Operation
+};
+
