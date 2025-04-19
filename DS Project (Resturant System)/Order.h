303a9000ba17d93bfb7a9eb874bdd86a0d1a707d
Mohamed A. Ibrahim <s-mohamed.elrehim@zewailcity.edu.eg>
2025-04-19 17:53:54 +0200

    Order (Last)

--- DS Project (Resturant System)/Order.h
+++ DS Project (Resturant System)/Order.h
@@ -1,30 +1,80 @@
 #pragma once
 #include <iostream>
 #include "string.h"
-#include "Node.h"
+
 using namespace std;
 
-template <typename T>
-class Order : Node<T>
+enum Type
+{
+	Normal=1, Vegan=2, VIP=3
+};
+class Order 
 {
 public:
-	Order(string n, double p, int rt, int s);
+	Order(string n, double p, int rt, int s) 
+	{
+		OrderType = stringToOrderType(n);
+		price = p;
+		RT = rt;
+		Size = s;
+
+
+		///Priority Setting///
+		if (OrderType == 3)
+		{
+			Priority = ((price / 1500) * 100 + (Size / 10) * 100 + WT); // Assuming Price range of 1500 EGP, Size of maximum 10 Orders
+		}
+	}
 	~Order() {};
-	void setNext(Order* N);
-	void setPrice(double p);
-	Order* getNext() const;
-	double getPrice() const;
-	string getType() const;
-	int getSize() const;
-	void setRT(int rt);
-	int getRT() const;
-	void setSize(int s);
+	void setPrice(double p)
+	{
+		price = p;
+	}
+	double getPrice() const
+	{
+		return price;
+	}
+	int getType() const
+	{
+		return OrderType;
+	}
+	int getSize() const
+	{
+		return Size;
+	}
+	void setRT(int rt) 
+	{
+		RT = rt;
+	}
+	int getRT() const
+	{
+		return RT;
+	}
+	void setWT(int currentTime)
+	{
+		WT = (currentTime - RT);
+	}
+	int getWT() const 
+	{
+		return WT;
+	}
+	void setSize(int s)
+	{
+		Size = s;
+	}
+	Type stringToOrderType(const string& str) {
+		if (str == "Normal") return Normal;
+		else if (str == "Vegan") return Vegan;
+		else if (str == "VIP") return VIP;
+		else return;
+	}
 
 private:
 	double price = 0;
-	string Type = "";
-	Order* Next = NULL;
+	Type OrderType;
 	int RT;
+	int WT;
 	int Size;
+	double Priority;
 };
 
---
+++ DS Project (Resturant System)/PriOrder.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include "string.h"
+#include "priNode.h"
+
+template <typename T>
+class Order : priNode<T>
+{
+public:
+	Order(string n, double p, int rt, int s);
+	~Order() {};
+	void setNext(Order* N);
+	void setPrice(double p);
+	Order* getNext() const;
+	double getPrice() const;
+	string getType() const;
+	int getSize() const;
+	void setRT(int rt);
+	int getRT() const;
+	void setSize(int s);
+
+private:
+	double price = 0;
+	string Type = "";
+	Order* Next = NULL;
+	int RT;
+	int Size;
+};
+
