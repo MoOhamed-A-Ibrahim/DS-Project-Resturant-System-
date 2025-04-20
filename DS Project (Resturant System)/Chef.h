c58451ea2feaa41d1603b0ef26feec780fd4b1c4
Mohamed A. Ibrahim <s-mohamed.elrehim@zewailcity.edu.eg>
2025-04-20 18:18:16 +0200

    Chef Fixed

--- DS Project (Resturant System)/Chef.h
+++ DS Project (Resturant System)/Chef.h
@@ -1,81 +1,26 @@
 #pragma once
-#include "LinkedQueue.h"
-#include "PriQueue.h"
-#include "Order.h"
-#include "Chef.h"
-#include <fstream>
 
+#include <iostream>
+#include <string>
 using namespace std;
 
-class Resturant
+
+class Chef
 {
 public:
-	Resturant()
+	Chef(string T, int s)
 	{
+		CookType = stringToOrderType(T);
+		CookSpeed = s;
 	}
-	~Resturant()
+	~Chef() {}
+	Type stringToOrderType(const string& str)
 	{
-
+		if (str == "Normal") return Normal;
+		else if (str == "Vegan") return Vegan;
+		else if (str == "VIP") return VIP;
 	}
-
-	// Use All functions to run the game //
-	void Run()
-	{
-
-	}
-
-	// Read Line From File // 
-	void ReadFromFile(const string& FileName)
-	{
-		ifstream inputFile(FileName);
-
-
-	}
-
-
-	// Assign Waiting Orders Loaded From File to Waiting Lists//
-	void ASSignWaiting(string Name, double Price, int Size)
-	{
-		Order* NewOrder = new Order(Name, Price, CurrentTime, Size);
-		int type = NewOrder->stringToOrderType(Name);
-		switch (type)
-		{
-		case 1:
-			WaitingNormal.enqueue(NewOrder);
-			break;
-		case 2:
-			WaitingVegan.enqueue(NewOrder);
-			break;
-		case 3:
-			WaitingVIP.enqueue(NewOrder, NewOrder->getPriority());
-			break;
-		}
-
-	}
-
-
-	void PutInService() {}
-
-
 private:
-
-	int CurrentTime;
-	int VIPcooks;
-	int NormalCooks;
-	int VeganCooks;
-
-	///  Orders Lists ///
-
-	LinkedQueue<Order*> AllOrdersList;
-	LinkedQueue<Order*> WaitingVegan;
-	LinkedQueue<Order*> WaitingNormal;
-	LinkedQueue<Order*> DeliveredOrders;
-	priQueue<Order*> WaitingVIP;
-	priQueue<Order*> InserviceOrders;
-
-	//// Cooks Lists ///
-
-	LinkedQueue<Chef*> ReadyVIP;
-	LinkedQueue<Chef*> ReadyVegan;
-	LinkedQueue<Chef*> ReadyNormal;
+	int CookSpeed;
+	Type CookType;
 };
